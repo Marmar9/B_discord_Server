7968c70cc91442b346905bdf96f27a8810110f8d
Marcin Chiliński <marcin.chilinski.mobile@o2.pl>
2023-09-13 21:53:25 +0200

    d_1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import { Elysia } from "elysia";
 import {Server, type ServerOptions} from "socket.io"
-import {serve} from "bun"
+import {Client} from "cassandra-driver"
+
+// CREATE TABLE test (
+//     id int PRIMARY KEY,
+//     name text,
+//     age int
+//     );
+
+
+
+const client = new Client({
+  contactPoints: ['192.168.1.231:9042'],
+  keyspace: 'my_keyspace',
+  
+})
 const app = new Elysia().get("/", () => "Hello Elysia").listen(Bun.env.A as string | number);
 const io = new Server()
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
